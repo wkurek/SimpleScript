1c6ba59d9bd08397c24ec512c9c8a8eeea324ee1
wkurek <w.kurek2@gmail.com>
2019-04-17 15:36:55 +0200

    providing assignment consistency across object/primitive/function identifier

--- assignment.cpp
+++ assignment.cpp
@@ -4,22 +4,31 @@ OperationExpressionAssignment::~OperationExpressionAssignment() {
     delete this->expression;
 }
 
-void OperationExpressionAssignment::evaluate(Object scope) {
-    Variable var = this->expression.evaluate(scope);
+void OperationExpressionAssignment::evaluate(Object scope) const {
+    Variable var = this->expression->evaluate(scope);
 
     if(var.getType() == PRIMITIVE) {
         scope.getPrimitive(this->identifier) = var.getPrimitive();
+
+        scope.removeObject(this->identifier);
+        scope.removeFunction(this->identifier);
     } else if(var.getType() == OBJECT) {
         scope.getObject(this->identifier) = var.getObject();
+
+        scope.removePrimitive(this->identifier);
+        scope.removeFunction(this->identifier);
     } else if(var.getType() == FUNCTION) {
         scope.getFunction(this->identifier) = var.getFunction();
+
+        scope.removePrimitive(this->identifier);
+        scope.removeObject(this->identifier);
     }
 }
 
-void FunctionAssignment::evaluate(Object scope) {
+void FunctionAssignment::evaluate(Object scope) const {
     scope.getFunction(this->identifier) = this->funct;
 }
 
-void ObjectAssignment::evaluate(Object scope) {
+void ObjectAssignment::evaluate(Object scope) const {
     scope.getObject(this->identifier) = this->object;
 }
