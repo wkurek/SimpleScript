c901ea6985cc1caebdb4060c8908be5c52bd3d78
wkurek <w.kurek2@gmail.com>
2019-05-03 10:51:38 +0200

    program workflow

--- SimpleScript/SimpleScript.cpp
+++ SimpleScript/SimpleScript.cpp
@@ -7,12 +7,21 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc > 1) {
+	if (argc <= 1) {
+		if (argv[0])
+			std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+		else
+			std::cerr << "Usage: <program name> <file_path>" << std::endl;
+
+		exit(1);
+	} else if (argc > 1) {
 		fopen_s(&yyin, argv[1], "r+");
 
 		if (yyin == NULL) {
-			cerr << "error: could not open file \"" << argv[1] << "\" for reading" << endl;
-			return 1;
+			std::cerr << "error: could not open file \"" << argv[1] << "\" for reading" << std::endl;
+			std::cerr << "Usage: <program name> <file_path>" << std::endl;
+			
+			exit(1);
 		}
 
 		yyparse();
